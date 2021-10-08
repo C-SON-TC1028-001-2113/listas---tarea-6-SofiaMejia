80c5e2815d9edeb5eb34cafc48d4b25fd1187896
SofiaMejia <A01252540@itesm.mx>
2021-10-08 04:16:21 +0000

    17 menores a numero

--- assignments/17MenoresANumero/src/exercise.py
+++ assignments/17MenoresANumero/src/exercise.py
@@ -1,7 +1,27 @@
 
+def crear_matriz():
+    R = int(input())
+    C = int(input())
+    matriz = []
+    for i in range(R):
+        matriz.append([])
+        for j in range(C):
+            matriz[i].append(int(input()))
+    return matriz
+
+def menor_diez(matriz):
+    resultado = []
+    for i in range(len(matriz)):
+        for j in range(len(matriz[0])):
+            if matriz[i][j]<10:
+                resultado.append(matriz[i][j])
+    return resultado
 
 def main():
-   
+    matriz = crear_matriz()
+    resultado = menor_diez(matriz)
+    print(resultado)
+
 
 
 
